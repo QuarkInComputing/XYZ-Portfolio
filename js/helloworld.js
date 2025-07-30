00001472bfb9425fed142516c26a122fad39969f
ThirdTitan <jackmcleanwilson@gmail.com>
2025-07-30 22:00:50 +0100

    Added letter by letter

---
+++ js/helloworld.js
@@ -0,0 +1,28 @@
+const display = document.getElementById('helloworld');
+
+const aboutButton = document.getElementById('aboutbutton');
+const myworkButton = document.getElementById('myworkbutton');
+const blogButton = document.getElementById('blogbutton');
+
+let isAnimating = false;
+
+function helloWorld(text) {
+    if (isAnimating) return; 
+
+    isAnimating = true;
+    let index = 1;
+
+    const interval = setInterval(() => {
+        if (index > text.length) {
+            clearInterval(interval);
+            isAnimating = false; 
+            return;
+        }
+        display.textContent = text.substring(0, index);
+        index++;
+    }, 50);
+}
+
+aboutButton.addEventListener('mouseover', () => helloWorld("About Me"));
+myworkButton.addEventListener('mouseover', () => helloWorld("My Work"));
+blogButton.addEventListener('mouseover', () => helloWorld("The Blog"));
